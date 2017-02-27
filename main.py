6e55963d824f6a26ba8693e7470147ead6c30d75
MaryP <mlparsons@zoho.com>
2017-02-26 19:45:47 -0600

    creation of tempaltes and completion of project

--- main.py
+++ main.py
@@ -14,12 +14,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2, jinja2, cgi, os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPosts(db.Model):
+    title = db.StringProperty(required = True)
+    post_content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Index(Handler):
+    def get (self):
+        page = """<center> <a href="/blog"> Build-a-blog by Mary </a></center>"""
+        self.response.write(page)
+
+class PostIndex(Handler):
+
+    def render_index(self, title="", post_content=""):
+        posts = db.GqlQuery("SELECT * FROM BlogPosts ORDER BY created DESC LIMIT 5")
+        self.render("index.html", title=title, post_content=post_content, posts=posts)
     def get(self):
-        self.response.write('Hello world!')
+        self.render_index()
+
+class NewPost(Handler):
+    def write_form(self, title="", post_content="", error=""):
+        self.render("newpost.html", title=title, post_content=post_content, error=error)
+
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        title = self.request.get("title")
+        post_content = self.request.get("post_content")
+
+        if title and post_content:
+            post = BlogPosts(title=title, post_content=post_content)
+            post.put()
+            d=int(post.key().id())
+            ###self.redirect("/blog/"+ str(post.key().id()))
+            self.redirect("/blog/%s" % d)
+        else:
+            error = "Please make sure to title and create your new entry!"
+            self.write_form(title, post_content, error)
+
+class PublishedPost(Handler):
+    def get(self, id):
+        int_id=int(id)
+        post = BlogPosts.get_by_id(int_id)
+        ###self.response.out.write(id)
+        self.render('post.html', post=post)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/blog', PostIndex),
+    ('/blog/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', PublishedPost)
+
+###    ("/published", PublishedPost)
+
 ], debug=True)
